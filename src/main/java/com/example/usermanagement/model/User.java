82bc48b4c112046ef7c3a118287fae82461fe393
Alesilvar <alesilvar.1074@gmail.com>
2024-10-06 21:48:52 -0500

    properties con variables de entorno

--- src/main/java/com/example/usermanagement/model/User.java
+++ src/main/java/com/example/usermanagement/model/User.java
@@ -11,6 +11,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/usermanagement/repository/RoleRepository.java
+++ src/main/java/com/example/usermanagement/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.example.usermanagement.repository;
 
 import com.example.usermanagement.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
